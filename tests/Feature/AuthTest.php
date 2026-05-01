4c2633a3fefc7ea7e43e581b98ab23bd52fcbcbb
Zwe Sithu <withzues@gmail.com>
2026-05-02 00:07:27 +0630

    test: chore rename test cases

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -1,18 +1,18 @@
 <?php
 
-test('/login returns a successful response', function () {
+test('login endpoint returns a successful response', function () {
     $response = $this->get('/login');
 
     $response->assertOk();
 });
 
-test('/register returns a successful response', function () {
+test('register endpoint returns successful response', function () {
     $response = $this->get('/register');
 
     $response->assertOk();
 });
 
-test('/ return login redirect while unauthenticated', function () {
+test('guest is redirected from home to login', function () {
     $response = $this->get('/');
 
     $response->assertStatus(302);
